5203527a5f6c9f8eaae29cd8a6d81a624b9591cb
Santiago López Rey <devsantilopez@gmail.com>
2022-04-02 12:55:14 +0200

    Base time modifier spawn function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject plusTime;
+
+    public GameObject subtractTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnTimeModifier();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnTimeModifier(){
+
+        Vector2 randomPos = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+
+        int randomPrefab = Random.Range(0,2);
+
+        if(randomPrefab==0){
+
+            Instantiate(plusTime, randomPos, Quaternion.identity);
+        } else {
+            Instantiate(subtractTime, randomPos, Quaternion.identity);
+        }
     }
 }
