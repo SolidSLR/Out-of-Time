98ce66b1313c2a7008e5fce312367662d0b7dd35
Santiago López Rey <devsantilopez@gmail.com>
2022-04-02 13:10:48 +0200

    Base spawn coroutine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,13 @@ public class GameManager : MonoBehaviour
     public GameObject plusTime;
 
     public GameObject subtractTime;
+
+    // Create the game timer
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnTimeModifier();
+        StartCoroutine("SpwanCorout");
     }
 
     // Update is called once per frame
@@ -21,7 +24,7 @@ public class GameManager : MonoBehaviour
 
     private void SpawnTimeModifier(){
 
-        Vector2 randomPos = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        Vector2 randomPos = new Vector2(Random.Range(-6.4f, 6.5f), Random.Range(-5f, 5f));
 
         int randomPrefab = Random.Range(0,2);
 
@@ -30,6 +33,26 @@ public class GameManager : MonoBehaviour
             Instantiate(plusTime, randomPos, Quaternion.identity);
         } else {
             Instantiate(subtractTime, randomPos, Quaternion.identity);
+        }
+    }
+
+    private IEnumerator SpwanCorout(){
+
+        // Modify condition to use the game and the player timers
+
+        int j = 0;
+
+        while(j <5){
+
+            // Randomize seconds of waiting
+
+            float seconds = Random.Range(1f, 2.5f);
+
+            yield return new WaitForSeconds(seconds);        
+
+            SpawnTimeModifier();
+
+            j++;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
 
     private float speed = 5f;
+
+    // Create the player timer
+    
     // Start is called before the first frame update
     void Start()
     {
