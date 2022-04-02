d9c44e80c471be190e89beff697270081126d07b
Santiago López Rey <devsantilopez@gmail.com>
2022-04-02 12:40:55 +0200

    Player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.W)){
+
+            rb.velocity = Vector2.up * speed;
+        } else if(Input.GetKey(KeyCode.S)){
+
+            rb.velocity = Vector2.down * speed;
+        }else if(Input.GetKey(KeyCode.D)){
+            
+            rb.velocity = Vector2.right * speed;
+        }else if(Input.GetKey(KeyCode.A)){
+
+            rb.velocity = Vector2.left * speed;
+        }else{
+            rb.velocity = new Vector2(0,0);
+        }
+    }
+}
